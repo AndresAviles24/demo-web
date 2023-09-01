f807c8efdfe14c87c187395b84c4b699129ee9f9
AndresAviles24 <andres.aviles09@gmail.com>
2023-09-01 18:49:22 -0400

    Hello World

---
+++ src/main/java/bo/edu/ucb/demoweb/controller/DemoController.java
@@ -0,0 +1,12 @@
+package bo.edu.ucb.demoweb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
